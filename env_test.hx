484cae6833d0606fc68c8914fad3905a86999b3a
Tyler Wright <tyler.david.wright@gmail.com>
2021-04-18 10:45:21 +0100

    Added Windows build

--- env_test.hx
+++
@@ -1,9 +0,0 @@
-view_dimension:2
-view_rect:-10 -10 20 20
-view_elev:30
-view_azim:-60
-view_cuboid:-10 -10 -10 20 20 20
-points_per_unit:2
-eq0:{'text': 'a = 0', 'colour': '224 82 99', 'locked': 0, 'hidden': 0, 'contoured': 0}
-eq1:{'text': 'f(x) = x^2 + a', 'colour': '3 247 235', 'locked': 0, 'hidden': 0, 'contoured': 0}
-eq2:{'text': '0 = f(x)', 'colour': '100 87 166', 'locked': 0, 'hidden': 0, 'contoured': 0}
